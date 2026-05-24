75a5fb473513062b1913d9b6285c47e8ecc7d20d
ronaktanwar <ronaktanwar.business@gmail.com>
2026-05-24 17:10:53 +0530

    add : collection schema

---
+++ src/database/schemas/collection.schema.ts
@@ -0,0 +1,46 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Product } from './product.schema';
+
+export type CollectionDocument = HydratedDocument<Collection>;
+
+@Schema({
+  timestamps: true,
+  collection: 'collections',
+})
+export class Collection {
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  name: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  })
+  slug: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+    default: [],
+  })
+  product_ids: mongoose.Types.ObjectId[];
+
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  banner_img: string;
+
+  @Prop({
+    enum: ['ACTIVE', 'INACTIVE'],
+    default: 'ACTIVE',
+    uppercase: true,
+  })
+  status: string;
+}
+
+export const CollectionSchema = SchemaFactory.createForClass(Collection);
