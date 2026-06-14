dcf45c410a6d9bc67cf41a00c80f57935310f5cc
ronaktanwar <ronaktanwar.business@gmail.com>
2026-06-14 21:35:32 +0530

    fix

--- src/main.ts
+++ src/main.ts
@@ -61,7 +61,7 @@ async function bootstrap() {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'PROD',
         maxAge: 1000 * 60 * 60 * 24,
-        sameSite: 'lax', // ⬅️ add this — needed for cross-origin cookie sending
+        sameSite: 'none', // ⬅️ add this — needed for cross-origin cookie sending
       },
     }),
   );
